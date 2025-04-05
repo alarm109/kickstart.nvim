6410ed7473abb056e8ca8ac0a0523964a8c7a074
Matas <matas109@gmail.com>
2025-04-05 19:36:34 +0300

    feat: add keymaps for list next and prev

--- init.lua
+++ init.lua
@@ -171,6 +171,8 @@ vim.keymap.set('i', 'jj', '<Esc>')
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set('n', '∆', '<cmd>lnext<CR>')
+vim.keymap.set('n', '˚', '<cmd>lprev<CR>')
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
